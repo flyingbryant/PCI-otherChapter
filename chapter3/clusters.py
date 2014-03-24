cf58ad3d16adcadd1766d09e13998c3e84b6723d
flyingbryant <wff20061023@126.com>
2014-03-24 18:23:26 +0800

    third commit

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -7,9 +7,9 @@ def pearson(v1,v2):
 	sum1Sq=sum([pow(v,2) for v in v1])
 	sum2Sq=sum([pow(v,2) for v in v2])
 
-	pSum=sum([v1[i]*v2[i] for i in range(len(v1)))
+	pSum=sum([v1[i]*v2[i] for i in range(len(v1))])
 	num = pSum-(sum1*sum2/len(v1))
-	den = sqrt((sum1Sq-pow(sum,2)/len(v1)*(sum2Sq-pow(sum2,2))/len(v1)))i
+	den = sqrt((sum1Sq-pow(sum1,2)/len(v1)*(sum2Sq-pow(sum2,2))/len(v1)))
 	if den==0: return 0
 	return 1.0-num/den
 
@@ -24,7 +24,7 @@ def readfile(filename):
 		p=line.strip().split('\t')
 		rownames.append(p[0])
 		data.append([float(x) for x in p[1:]])
-	return rownames,columns,data
+	return rownames,colnames,data
 
 class bicluster:
 	def __init__(self,vec,left=None,right=None,distance=0.0,id=None):
@@ -47,7 +47,7 @@ def hcluster(rows,distance=pearson):
 		for i in range(len(clust)):
 			for j in range(i+1,len(clust)):
 				if(clust[i].id, clust[j].id) not in distances:
-					distances[(clust[i].id,clust[j].id)]=distance[clust[i].vec,clust[j].vec]
+					distances[(clust[i].id,clust[j].id)]=distance(clust[i].vec,clust[j].vec)
 
 				d=distances[(clust[i].id,clust[j].id)]
 
@@ -56,9 +56,9 @@ def hcluster(rows,distance=pearson):
 					lowestpair=(i,j)
 
 		
-		mergevec=[(clust[lowestpair[0]].vec[i]+clust[lowestpair[1]].vec[i])/2.0 for in range(len(clust[0].vec))]
+		mergevec=[(clust[lowestpair[0]].vec[i]+clust[lowestpair[1]].vec[i])/2.0 for i in range(len(clust[0].vec))]
 
-		newclust=bicluster(mergevec.left=clust[lowestpair[0]],right=clust[lowestpair[1]],distance=closest,id=currentclustid)
+		newcluster=bicluster(mergevec,left=clust[lowestpair[0]],right=clust[lowestpair[1]],distance=closest,id=currentclustid)
 
 		currentclustid-=1
 		del clust[lowestpair[1]]
@@ -66,3 +66,34 @@ def hcluster(rows,distance=pearson):
 		clust.append(newcluster)
 
 	return clust[0]
+
+
+def printclust(clust,labels=None,n=0):
+	for i in range(n):print' ',
+	if clust.id<0:
+		print '_'
+	
+	else:
+		if labels==None: print clust.id
+		else: print labels[clust.id]
+
+	if clust.left!=None: 
+		printclust(clust.left, labels=labels,n=n+1)
+	if clust.right!=None:
+		printclust(clust.right,labels=labels,n=n+1)
+
+out=file('answer','w')
+def printToFile(clust,labels=None,n=0):	
+	for i in range(n):
+		out.write(' ',)
+	if clust.id<0:
+		out.write('_')
+	
+	else:
+		if labels==None: out.write(clust.id)
+		else: out.write(labels[clust.id])
+
+	if clust.left!=None: 
+		printToFile(clust.left, labels=labels,n=n+1)
+	if clust.right!=None:
+		printToFile(clust.right,labels=labels,n=n+1)
